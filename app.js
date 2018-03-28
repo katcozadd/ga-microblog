ee5ce599a0f7a2e9130a27128afe18d5f5ff6d1d
katcozadd <krosecozadd@gmail.com>
2018-03-27 20:03:48 -0600

    Figured out how to print text on screen, added some styling

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const form = document.getElementById('blogForm')
-const submit = document.getElementById('submitButton').value;
-const li = document.getElementById('myList')
+//const submit = document.getElementById('submitButton').value;
 
 form.addEventListener('submit', function(event) {
 	event.preventDefault();
 	const inputValue = document.getElementById('textBox').value;
-	
-
+	const myList = document.getElementById('myList')
+	myList.innerHTML += inputValue;
 console.log(inputValue);
 });
 
